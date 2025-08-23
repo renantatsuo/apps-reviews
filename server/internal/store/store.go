597930e39c88d29a86b5437ec9ce8f869c50dc63
renantatsuo <tatsuorenan@gmail.com>
2025-08-23 14:48:16 +0100

    implement basic key-value store

---
+++ server/internal/store/store.go
@@ -0,0 +1,38 @@
+package store
+
+import (
+	"fmt"
+	"sync"
+)
+
+type Store struct {
+	mu    sync.RWMutex
+	store map[string]any
+}
+
+func New() *Store {
+	return &Store{
+		store: make(map[string]any),
+	}
+}
+
+// Get retrieves a value from the store.
+func Get[T any](key string, s *Store) (T, error) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	values, ok := s.store[key]
+	if !ok {
+		return *new(T), fmt.Errorf("key %s not found", key)
+	}
+
+	return values.(T), nil
+}
+
+// Set stores a value in the store.
+func Set[T any](key string, value T, s *Store) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	s.store[key] = value
+}
