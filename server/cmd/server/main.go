31e0b4f9b8fbfeac1b04e743c35b4292edf23fcb
renantatsuo <tatsuorenan@gmail.com>
2025-08-26 13:33:35 +0100

    add config module

--- server/cmd/server/main.go
+++ server/cmd/server/main.go
@@ -3,19 +3,19 @@ package main
 import (
 	"context"
 	"errors"
+	"log"
 	"log/slog"
 	"net/http"
 	"os"
 	"os/signal"
-	"strings"
 	"syscall"
 	"time"
 
+	"github.com/renantatsuo/app-review/server/internal/config"
 	"github.com/renantatsuo/app-review/server/internal/reviews"
 	"github.com/renantatsuo/app-review/server/internal/server"
 	"github.com/renantatsuo/app-review/server/internal/store"
 	"github.com/renantatsuo/app-review/server/pkg/apple"
-	"github.com/renantatsuo/envv"
 )
 
 const (
@@ -23,29 +23,20 @@ const (
 )
 
 func main() {
-	port := envv.Get("PORT").Int().Default(8080).Parse()
-	logLevelStr := envv.Get("LOG_LEVEL").String().Default("debug").Parse()
-	// defaults to hevy and instagram
-	appleAppIDsStr := envv.Get("APP_IDS").String().Default("1458862350,389801252").Parse()
-	appleAppIDs := strings.Split(appleAppIDsStr, ",")
-	storeDir := envv.Get("STORE_DIR").String().Default("data").Parse()
-	reviewsTimeLimit := envv.Get("REVIEWS_TIME_LIMIT").Duration().Default(48 * time.Hour).Parse()
-	pollingInterval := envv.Get("POLLING_INTERVAL").Duration().Default(30 * time.Second).Parse()
-
-	logLevel, err := parseLogLevel(logLevelStr)
+	config, err := config.LoadConfigFromEnv()
 	if err != nil {
-		panic(err)
+		log.Fatalf("error loading config: %w", err)
 	}
 
 	l := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
-		Level: logLevel,
+		Level: config.LogLevel,
 	}))
 
-	l.Info("initializing server", "port", port, "logLevel", logLevel)
+	l.Info("initializing server", "port", config.Port, "logLevel", config.LogLevel)
 
 	store := store.New()
 	appleClient := apple.New()
-	reviewsClient := reviews.New(l, store, appleClient, pollingInterval, reviewsTimeLimit, storeDir, appleAppIDs)
+	reviewsClient := reviews.New(l, store, appleClient, config.PollingInterval, config.ReviewsTimeLimit, config.StoreDir, config.AppIDs)
 	reviewsClient.Load()
 
 	ctx, cancel := context.WithCancel(context.Background())
@@ -54,7 +45,7 @@ func main() {
 		reviewsClient.StartPolling(ctx)
 	}()
 
-	s := server.New(port, l, reviewsClient)
+	s := server.New(config.Port, l, reviewsClient)
 
 	go func() {
 		if err := s.Start(); err != nil && !errors.Is(err, http.ErrServerClosed) {
@@ -79,9 +70,4 @@ func main() {
 		l.Error("error stopping server", "error", err)
 		os.Exit(1)
 	}
-}
-
-func parseLogLevel(logLevel string) (l slog.Level, err error) {
-	err = l.UnmarshalText([]byte(logLevel))
-	return
 }
